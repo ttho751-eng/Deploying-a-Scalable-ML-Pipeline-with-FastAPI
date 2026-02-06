e07356644ea64431db6da3bf2f01bfcf646c7ff6
Tarynn <tarynnthompson@TarynnsMac.local>
2026-02-06 11:09:29 -0700

    Define model artifact paths and save trained artifacts

--- train_model.py
+++ train_model.py
@@ -15,12 +15,18 @@ from ml.model import (
 # TODO: load the cencus.csv data
 project_path = os.path.dirname(os.path.abspath(__file__))
 data_path = os.path.join(project_path, "data", "census_clean.csv")
-print(data_path)
-data = None # your code here
+model_path = os.path.join(project_path, "model", "model.pkl")
+encoder_path = os.path.join(project_path, "model", "encoder.pkl")
+lb_path = os.path.join(project_path, "model", "lb.pkl")
+
+print(data_path) 
+data = pd.read_csv(data_path)
+
 
 # TODO: split the provided data to have a train dataset and a test dataset
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = None, None# Your code here
+train, test = train_test_split(data, test_size=0.20, random_state=42)
+
 
 # DO NOT MODIFY
 cat_features = [
@@ -36,11 +42,11 @@ cat_features = [
 
 # TODO: use the process_data function provided to process the data.
 X_train, y_train, encoder, lb = process_data(
-    # your code here
-    # use the train dataset 
-    # use training=True
-    # do not need to pass encoder and lb as input
-    )
+    train,
+    categorical_features=cat_features,
+    label="salary",
+    training=True,
+)
 
 X_test, y_test, _, _ = process_data(
     test,
@@ -52,13 +58,12 @@ X_test, y_test, _, _ = process_data(
 )
 
 # TODO: use the train_model function to train the model on the training dataset
-model = None # your code here
+model = train_model(X_train, y_train)
 
-# save the model and the encoder
-model_path = os.path.join(project_path, "model", "model.pkl")
 save_model(model, model_path)
-encoder_path = os.path.join(project_path, "model", "encoder.pkl")
 save_model(encoder, encoder_path)
+save_model(lb, lb_path)
+
 
 # load the model
 model = load_model(
@@ -66,7 +71,8 @@ model = load_model(
 ) 
 
 # TODO: use the inference function to run the model inferences on the test dataset.
-preds = None # your code here
+preds = inference(model, X_test)
+
 
 # Calculate and print the metrics
 p, r, fb = compute_model_metrics(y_test, preds)
@@ -74,14 +80,22 @@ print(f"Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}")
 
 # TODO: compute the performance on model slices using the performance_on_categorical_slice function
 # iterate through the categorical features
+open("slice_output.txt", "w").close()
 for col in cat_features:
     # iterate through the unique values in one categorical feature
     for slicevalue in sorted(test[col].unique()):
         count = test[test[col] == slicevalue].shape[0]
         p, r, fb = performance_on_categorical_slice(
-            # your code here
-            # use test, col and slicevalue as part of the input
-        )
+    test,
+    model,
+    encoder,
+    lb,
+    categorical_features=cat_features,
+    slice_col=col,
+    slice_value=slicevalue,
+)
+
         with open("slice_output.txt", "a") as f:
             print(f"{col}: {slicevalue}, Count: {count:,}", file=f)
             print(f"Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}", file=f)
+
